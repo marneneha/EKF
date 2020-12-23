be68ad51d06c5ee017206334e6724bcbf29ca988
nehamarne27 <nehamarne27@gmail.com>
2020-12-23 23:47:37 +0530

    motion model calculation  filtering

--- src/lidar_ekf/lidar_ekf.py
+++ src/lidar_ekf/lidar_ekf.py
@@ -17,13 +17,16 @@ tran_vel = data['v']
 tran_vel_var = data['v_var']
 rot_vel = data['om']
 rot_vel_var = data['om_var']
+motion_var = [[tran_vel_var],[rot_vel_var]]
 pose_landmark = data['l']
 bearing = data['b']
 bearing_var = data['b_var']
 range_reading = data['r']
 range_var = data['r_var']
+corr_var = [[range_var],[bearing_var]]
 X_arr = []
 Y_arr = []
+P = 0
 for i in range(len(time)-1):
 	dt = time[i+1]-time[i]
 	x = x - (tran_vel[i]/rot_vel[i])*np.sin(theta) + (tran_vel[i]/rot_vel[i])*np.sin(theta+rot_vel[i]*dt)
@@ -31,9 +34,18 @@ for i in range(len(time)-1):
 	y = y + (tran_vel[i]/rot_vel[i])*np.cos(theta) - (tran_vel[i]/rot_vel[i])*np.cos(theta+rot_vel[i]*dt)
 	Y_arr.append(y)
 	theta = theta+rot_vel[i]*dt
+	for j in range(len(pose_landmark)):
+		px = pose_landmark[j,0]-x
+		py = pose_landmark[j,1]-y
+		H = [[px/np.sqrt(px**2+py**2),py/np.sqrt(px**2+py**2)],[-py/(px**2+py**2),px/(px**2+py**2)]]
+		P = P +motion_var
+		Kg = P*Ht/(H*P*Ht+corr_var)
+		h = [[np.sqrt(px**2+py**2)],[np.arctan(py/px)-theta]]
+		Z = [[range_reading[i], bearing[i]]
+		x = x + Kg*(Z[0]-h[0])
+		y = y + Kg*(Z[1]-h[1])
+		P = (1-Kg*H)*P
 
-
-h[1] = sqrt()	
 print("plot for motion model only")
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
