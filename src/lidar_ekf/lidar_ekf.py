6edead7827d85780e40f2b8be8cdc044b27ed987
nehamarne27 <nehamarne27gmail,com>
2020-12-27 19:18:51 +0530

    just addition

--- src/lidar_ekf/lidar_ekf.py
+++ src/lidar_ekf/lidar_ekf.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 with open('data.pickle', 'rb') as k:
     data=pickle.load(k)
+print(data)
 time = data['t']
 x_init = data['x_init']
 y_init = data['y_init']
@@ -25,33 +26,49 @@ range_var = data['r_var']
 corr_var = np.diag([range_var,bearing_var])
 X_arr = []
 Y_arr = []
-P = np.zeros(3)
+P = np.diag([1,1,0.1])
 I =np.identity(3)
+state = np.array([[x],[y],[theta]])
+print(state)
 neha = np.array([[0],[0],[0]])
-for i in range(len(time)-1):
+#(len(time)/5.00)-1.00
+for i in range(50):
 	dt = time[i+1]-time[i]
+	print("x is",x)
+	print((tran_vel[i]/rot_vel[i])*np.sin(theta))
+	print((tran_vel[i]/rot_vel[i])*np.sin(theta+rot_vel[i]*dt))
 	x = x - (tran_vel[i]/rot_vel[i])*np.sin(theta) + (tran_vel[i]/rot_vel[i])*np.sin(theta+rot_vel[i]*dt)
-	X_arr.append(x)
+	print("x is",x)
 	y = y + (tran_vel[i]/rot_vel[i])*np.cos(theta) - (tran_vel[i]/rot_vel[i])*np.cos(theta+rot_vel[i]*dt)
-	Y_arr.append(y)
 	theta = theta+rot_vel[i]*dt
+	state = np.array([[x],[y],[theta]])
+	print("for i in i",x,y,theta)
 	for j in range(len(pose_landmark)):
 		px = pose_landmark[j,0]-x
 		py = pose_landmark[j,1]-y
+		print(px, py)
 		h = np.array([[np.sqrt(px**2+py**2)],[np.arctan(py/px)-theta]], dtype='float')
-		H = np.matrix([[px/np.sqrt(px**2+py**2),py/np.sqrt(px**2+py**2),0],[-py/(px**2+py**2),px/(px**2+py**2),-1]], dtype='float')
+		print("h is",h)
 		A = np.matrix([[1,0,(-(tran_vel[i]/rot_vel[i])*np.cos(theta)+(tran_vel[i]/rot_vel[i])*np.cos(theta+rot_vel[i]*dt))],[0,1,(-(tran_vel[i]/rot_vel[i])*np.sin(theta)+(tran_vel[i]/rot_vel[i])*np.sin(theta+rot_vel[i]*dt))],[0,0,1]], dtype='float')
-		P = (A.dot(P).dot(A.T))+motion_var
+		P = A.dot(P.dot(A.T))+motion_var
+		H = np.matrix([[px/np.sqrt(px**2+py**2),py/np.sqrt(px**2+py**2),0],[-py/(px**2+py**2),px/(px**2+py**2),-1]], dtype='float')
 		Kg = P.dot(H.T).dot(np.linalg.inv((H.dot(P).dot(H.T)+corr_var)))
+		print("Kg is",Kg)
 		Z = np.array([[range_reading[i,j]], [bearing[i,j]]], dtype='float')
-		temp = Z-h
+		print("Z is",Z)
+		temp = [[0.01],[0.01]]
+		print("z-h is ",Z-h)
 		neha = Kg.dot(Z-h)
+		print("neha is",neha)
 		x = x + neha[0,0]
 		y = y + neha[1,0]
+		theta = theta + neha[2,0]
+		print("in the end",x,y,theta)
 		P = (I-Kg.dot(H)).dot(P)
 	print("i is",i)
 	print(x)
 	print(y)
+	state = np.array([[x],[y],[theta]])
 	X_arr.append(x)
 	Y_arr.append(y)
 fig = plt.figure()
